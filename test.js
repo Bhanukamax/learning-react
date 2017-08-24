2f9da15cf217723f8a3b647509660c23f162a4a2
bhanuka <bhanukamax@gmail.com>
2017-08-24 21:12:09 +0530

    main files

---
+++ test.js
@@ -0,0 +1,14 @@
+function App() {
+	return(
+	
+	<div>
+	<h1>Welcome from React Prototype Starter Kit !!</h1>
+	</div>
+	
+	);
+
+}
+ReactDOM.render(
+		<App />,
+	document.getElementById('root')
+);
